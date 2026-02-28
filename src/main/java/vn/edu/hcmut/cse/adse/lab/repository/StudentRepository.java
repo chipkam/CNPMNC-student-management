91d57ae34afb8dbd271b2a5d16d3502a1cec51b7
chipkam <khoachipnau@gmail.com>
2026-02-28 12:01:47 +0700

    Lab 3: Full tasks

--- src/main/java/vn/edu/hcmut/cse/adse/lab/repository/StudentRepository.java
+++ src/main/java/vn/edu/hcmut/cse/adse/lab/repository/StudentRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import vn.edu.hcmut.cse.adse.lab.entity.Student;
 
+import java.util.List;
+
 @Repository
 public interface StudentRepository extends JpaRepository<Student, String> {
+    List<Student> findByNameContainingIgnoreCase(String keyword);
 }
